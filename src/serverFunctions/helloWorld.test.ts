85aaf87f26ee2f74eb41fc370ccdf9a80f6df58c
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2026-02-24 13:27:22 +0100

    Implement helloWorld.test

---
+++ src/serverFunctions/helloWorld.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { helloWorld } from './helloWorld';
+
+describe('hello world test', () => {
+    it('should return a string', () => {
+        expect(typeof helloWorld()).toBe("string");
+    });
+    it ('should not return an empty string', () => {
+        expect(helloWorld()).not.toBe("");
+    });
+    it('should return "Hello from Poly."', () => {
+        expect(helloWorld()).toBe("Hello from Poly.");
+    });
+});
