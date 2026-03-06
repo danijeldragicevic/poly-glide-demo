866e3b012a89699fa194611e9495918af12a61e0
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2026-03-06 08:41:46 +0100

    Create dailyForecastWebhook

---
+++ src/eventHandlers/dailyForecastWebhook.ts
@@ -0,0 +1,34 @@
+import { PolyWebhook } from "polyapi";
+
+export const polyConfig: PolyWebhook = {
+    context: "demo",
+    name: "dailyForecastWebhook",
+    description: "Receives a latitude and longitude and emits an event to trigger the daily weather forecast.",
+    visibility: "TENANT",
+    method: "POST",
+    eventPayloadTypeSchema: {
+        type: "object",
+        properties: {
+            latitude: {
+                type: "number",
+                description: "Geographic latitude of the location, in decimal degrees (between -90 and 90).",
+            },
+            longitude: {
+                type: "number",
+                description: "Geographic longitude of the location, in decimal degrees (between -180 and 180).",
+            },
+        },
+        required: ["latitude", "longitude"],
+    },
+    responseStatus: 200,
+    responsePayload: { message: "Webhook received." },
+    responseHeaders: {
+        "Content-Type": "application/json",
+    },
+    xmlParserOptions: {
+        enabled: false,
+        explicitArray: false,
+        trim: false,
+        normalizeTags: false,
+    },
+};
