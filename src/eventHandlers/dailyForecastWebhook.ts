ff90c0bc43a541b19045fe394726ca43bc94f8a2
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2026-03-06 13:18:30 +0100

    try to load security function id dynamically

--- src/eventHandlers/dailyForecastWebhook.ts
+++ src/eventHandlers/dailyForecastWebhook.ts
@@ -3,7 +3,7 @@ import poly, { PolyWebhook } from "polyapi";
 // const functionPath = poly.demo.validateForecastPayload.name;
 
 const { functions } = require("../.poly/lib/server/functions.js");
-const validateForecastPayload_Id = functions.find(([path]: [string]) => path === "demo.validateForecastPayload")?.[1];
+export const validateForecastPayload_Id = functions.find(([path]: [string]) => path === "demo.validateForecastPayload")?.[1];
 
 
 export const polyConfig: PolyWebhook = {
