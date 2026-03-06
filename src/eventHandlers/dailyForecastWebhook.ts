d14e026e922874312f09fbc5436677327907f302
Danijel Dragicevic <danijel.dragicevic@productdock.com>
2026-03-06 12:00:20 +0100

    update securityFunctoins config, set id as dummy string

--- src/eventHandlers/dailyForecastWebhook.ts
+++ src/eventHandlers/dailyForecastWebhook.ts
@@ -35,7 +35,7 @@ export const polyConfig: PolyWebhook = {
     },
     securityFunctions: [
         {
-            id: poly.demo.validateForecastPayload.name
+            id: "validateForecastPayload"
         }
     ]
 };
